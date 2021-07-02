8e33a47ff6d09085f75a41685158689a932699c8
lucasfraporti <80408351+lucasfraporti@users.noreply.github.com>
2021-07-02 15:36:53 -0300

    Create main.py

---
+++ Atividades/Atividade2/main.py
@@ -0,0 +1,16 @@
+from Aluno import Aluno
+from AlunoEM import AlunoEM
+from AlunoES import AlunoES
+
+Aluninho = Aluno('01', 'Carlos Souza', '1010')
+Aluninho.imprimir()
+
+print('-=-='*20)
+
+AluninhoEM = AlunoEM('02', 'Bernardo Azevedo', '2020', '9º ano')
+AluninhoEM.imprimir()
+
+print('-=-='*20)
+
+AluninhoES = AlunoES('03', 'Elton Munhoz', '1000', '5º semestre')
+AluninhoES.imprimir()
